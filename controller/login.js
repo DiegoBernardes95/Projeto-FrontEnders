5ee8cf526d9b0007d65f7395bfa14d5f030bb9af
dcb95 <dcb1618@gmail.com>
2022-11-21 10:53:58 -0300

    validação de email no login, arquivo css do cadastro renomeado

--- controller/login.js
+++ controller/login.js
@@ -4,21 +4,30 @@ const emailLogin = document.getElementById('emailLogin');
 const senhaLogin = document.getElementById('senhaLogin');
 const btnLogin = document.getElementById('btnLogin');
 
-btnLogin.addEventListener('click', function (event) {
-    event.preventDefault();
-    if (emailLogin.value == localStorage.email && senhaLogin.value == localStorage.senha) {
-        Swal.fire({
-            title: "Login efetuado!",
-            text: 'Você será redirecionado para a página inicial.',
-            icon: "success",
-            button: false,
-        });
-        setTimeout(function () { window.location = '../../index.html' }, 3100);
-    } else {
-        Swal.fire({
-            icon: 'error',
-            title: 'Oops...',
-            html: '<p>Usuário ou senha inválidos! Tente novamente.<br>Se não possui uma conta, cadastre-se!</p>',
-          })
-    }
+$(document).ready(function () {
+    $('form').validate({
+        rules: {
+            email: {
+                required: true,
+                email: true,
+            }
+        },
+        submitHandler: function (form) {
+            if (emailLogin.value == localStorage.email && senhaLogin.value == localStorage.senha) {
+                Swal.fire({
+                    title: "Login efetuado!",
+                    text: 'Você será redirecionado para a página inicial.',
+                    icon: "success",
+                    button: false,
+                });
+                setTimeout(function () { window.location = '../../index.html' }, 3100);
+            } else {
+                Swal.fire({
+                    icon: 'error',
+                    title: 'Oops...',
+                    html: '<p>Usuário ou senha inválidos! Tente novamente.<br>Se não possui uma conta, cadastre-se!</p>',
+                })
+            }
+        }
+    })
 })
